c20fe72eaaaaafa52697bef4c36ed97bef4c95f3
avanthika-s <avanthikas.cse@citchennai.net>
2023-01-06 10:27:36 +0530

    add file

---
+++ main.js
@@ -0,0 +1,255 @@
+window.addEventListener('load', init);
+
+//Load More Words
+function load_more_words()
+{
+	var xmlhttp;
+	if (window.XMLHttpRequest)
+	{
+		//  IE7+, Firefox, Chrome, Opera, Safari 
+		xmlhttp=new XMLHttpRequest();
+	}
+	else
+	{
+		// IE6, IE5 
+		xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+	}
+	xmlhttp.onreadystatechange=function()
+	{
+		if (xmlhttp.readyState==4 && xmlhttp.status==200)
+		{
+			words=xmlhttp.responseText.split("\n");
+		}
+	}
+	xmlhttp.open("GET","words.txt",true);
+	xmlhttp.send();
+}
+
+// Globals
+
+// Available Levels
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 1
+};
+
+// To change level
+const currentLevel = levels.medium;
+
+let time = currentLevel;
+let score = 0;
+let isPlaying;
+
+// DOM Elements
+const wordInput = document.querySelector('#word-input');
+const currentWord = document.querySelector('#current-word');
+const scoreDisplay = document.querySelector('#score');
+const timeDisplay = document.querySelector('#time');
+const message = document.querySelector('#message');
+const seconds = document.querySelector('#seconds');
+const highscoreDisplay = document.querySelector('#highscore');
+
+var words = [
+'a',
+'abandon',
+'abandoned',
+'ability',
+'able',
+'about',
+'above',
+'abroad',
+'absence',
+'absent',
+'absolute',
+'absolutely',
+'absorb',
+'abuse',
+'academic',
+'accent',
+'accept',
+'acceptable',
+'access',
+'accident',
+'accidental',
+'accidentally',
+'accommodation',
+'accompany',
+'according to',
+'account',
+'birthday',
+'biscuit',
+'bit',
+'bite',
+'bitter',
+'bitterly',
+'black',
+'blade',
+'blame',
+'blank',
+'blind',
+'block',
+'cancer',
+'candidate',
+'candy',
+'cannot',
+'cap',
+'capable',
+'capacity',
+'capital',
+'captain',
+'capture',
+'decide on',
+'decision',
+'declare',
+'decline',
+'decorate',
+'decoration',
+'decorative',
+'decrease',
+'deep',
+'deeply',
+'defeat',
+'defence',
+'defend',
+'define',
+'eat out',
+'eat up',
+'economic',
+'economy',
+'edge',
+'edition',
+'editor',
+'educate',
+'educated',
+'education',
+'effect',
+'effective',
+'gain',
+'gallon',
+'gamble',
+'gambling',
+'game',
+'gap',
+'garage',
+'garbage',
+'garden',
+'gas',
+'gasoline',
+'habit',
+'hair',
+'hairdresser',
+'half',
+'hall',
+'hammer',
+'inability',
+'inch',
+'incident',
+'include',
+'including',
+'income',
+'jump',
+'June',
+'junior',
+'justice',
+'menu',
+'mere',
+'merely',
+'mess',
+'message',
+'metal',
+'zero',
+'zone',
+'yawn',
+'yeah',
+'year',
+'yellow',
+];
+
+
+// Initialize Game
+function init() {
+    // Show number of seconds in UI
+    seconds.innerHTML = currentLevel;
+    // Load word from array
+    showWord(words);
+    // Start matching on word input
+    wordInput.addEventListener('input', startMatch);
+    // Call countdown every second
+    setInterval(countdown, 1000);
+    // Check game status
+    setInterval(checkStatus, 50);
+    // Fetch more words
+    setTimeout(load_more_words(),1000)
+  }
+  
+  // Start match
+  function startMatch() {
+    if (matchWords()) {
+      isPlaying = true;
+      time = currentLevel + 1;
+      showWord(words);
+      wordInput.value = '';
+      score++;
+    }
+    
+    // Highscore based on score value for Session Storage
+    if (typeof sessionStorage['highscore'] === 'undefined' || score > sessionStorage['highscore']) {
+      sessionStorage['highscore'] = score;
+    } else {
+      sessionStorage['highscore'] = sessionStorage['highscore'];
+    }
+  
+    // Prevent display of High Score: -1
+    if (sessionStorage['highscore'] >= 0) {
+    highscoreDisplay.innerHTML = sessionStorage['highscore'];
+    }
+  
+    // If score is -1, display 0
+    if (score === -1) {
+      scoreDisplay.innerHTML = 0;
+    } else {
+      scoreDisplay.innerHTML = score;
+    }
+  }
+  
+  // Match currentWord to wordInput
+  function matchWords() {
+    if (wordInput.value === currentWord.innerHTML) {
+      message.innerHTML = 'Correct!!!';
+      return true;
+    } else {
+      message.innerHTML = '';
+      return false;
+    }
+  }
+  
+  // Pick & show random word
+  function showWord(words) {
+    // Generate random array index
+    const randIndex = Math.floor(Math.random() * words.length);
+    // Output random word
+    currentWord.innerHTML = words[randIndex];
+  }
+  
+  // Countdown timer
+  function countdown() {
+    // Make sure time is not run out
+    if (time > 0) {
+      // Decrement
+      time--;
+    } else if (time === 0) {
+      // Game is over
+      isPlaying = false;
+    }
+    // Show time
+    timeDisplay.innerHTML = time;
+  }
+  
+  // Check game status
+  function checkStatus() {
+    if (!isPlaying && time === 0) {
+      message.innerHTML = 'Game Over!!!';
+      score = -1;
+    }
+}
